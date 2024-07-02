44e0d0746e4e55a99b7ebb807df3b85fe1f787c8
Pranav Raghu <73378019+Impaler343@users.noreply.github.com>
2024-07-02 13:30:04 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,67 @@
+#ifndef AWK_PARSER_H
+#define AWK_PARSER_H
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class Operation {
+  AND,
+  OR,
+  NONE
+};
+
+template <typename T>
+bool compare_op(const std::string &lhs, const T &rhs, const std::string &op) {
+  std::stringstream ss(lhs);
+}
+
+class FileParser {
+public:
+  FileParser(std::string &&filename, char delimiter = ' ');
+  const std::vector<std::vector<std::string>> &get_parsed() const;
+
+private:
+  std::vector<std::vector<std::string>> parsed;
+};
+
+class PatternParser {
+public:
+  PatternParser(const std::string &str);
+  bool check_pattern(const std::vector<std::string> &tokens, int row_num);
+  const std::vector<std::string> &get_pattern() const;
+
+private:
+  std::vector<std::string> vect_patterns;
+  Operation op_type = Operation::AND;
+};
+
+class Stmt {
+public:
+  virtual void run(const std::vector<std::string> &tokens) = 0;
+};
+
+class PrintStmt : public Stmt {
+public:
+  PrintStmt(std::string::const_iterator &it, const std::string &input);
+  void run(const std::vector<std::string> &tokens);
+
+private:
+  std::vector<int> fields;
+};
+
+class ActionParser {
+public:
+  ActionParser(const std::string &input);
+  void run(const std::vector<std::string> &tokens);
+
+private:
+  std::vector<Stmt *> stmts;
+};
+
+#endif
+
