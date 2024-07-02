6e66af913fe1819ce38a7595ab88f6728b585aca
Pranav Raghu <kingprawn5000@gmail.com>
2024-07-02 14:26:03 +0530

    Added sample.cpp

---
+++ Submissions/sample.cpp
@@ -0,0 +1,11 @@
+#include "header.h"
+
+FileParser::FileParser(std::string &&filename, char delimiter) {
+  std::ifstream fin(filename);
+
+  if (!fin) {
+    std::cerr << "Error opening file" << std::endl;
+    exit(0);
+  }
+
+//Let's go ahead and complete this!
